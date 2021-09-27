3ed8fc377714ecc98edb95994b22730794c344e7
Trevor Sears <trevors@knorrmarketing.com>
2021-09-27 17:03:00 -0400

    Stub implementation of `HTTPServer`.

--- ts/http-server.ts
+++ ts/http-server.ts
@@ -1,5 +1,25 @@
-export class HTTPServer {
+import http from "http";
+import { AbstractRouter } from "./routing/abstract-router";
+import { IncomingHTTPRequest } from "./messages/incoming-http-request";
+import { OutgoingHTTPResponse } from "./messages/outgoing-http-response";
 
+export class HTTPServer extends AbstractRouter {
 
+	protected server: http.Server;
+	
+	public constructor() {
+		
+		super();
+		
+		this.server = http.createServer();
+		
+		this.server.on("request", (rawRequest: http.IncomingMessage, rawResponse: http.ServerResponse): void => {
+		
+			let request: IncomingHTTPRequest = IncomingHTTPRequest.fromNodeIncomingMessage(rawRequest);
+			let response: OutgoingHTTPResponse = OutgoingHTTPResponse.fromNodeServerResponse(rawResponse);
+		
+		});
+		
+	}
 
 }
