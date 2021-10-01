4b0738be27caa943c1d82f5c2a9dd0e3cf9db997
Trevor Sears <trevors@knorrmarketing.com>
2021-10-01 16:37:18 -0400

    Added request fulfillment methods and more documentation.

--- ts/http-server.ts
+++ ts/http-server.ts
@@ -1,10 +1,12 @@
 import http from "http";
-import { HTTPStatusCode } from "@t99/http";
+import { HTTPStatusCode, HTTPVersionObject } from "@t99/http";
 import { AbstractRouter } from "./routing/abstract-router";
 import { IncomingHTTPRequest } from "./messages/incoming-http-request";
 import { OutgoingHTTPResponse } from "./messages/outgoing-http-response";
 import { MiddlewareFunction } from "./middleware/middleware";
 import { ClientAccessibleError } from "./error/client-accessible-error";
+
+// TODO [10/1/2021 @ 12:25 PM] Should these two methods be moved somewhere else? Their own file?
 
 export async function defaultUnhandledRequestHandler(request: IncomingHTTPRequest,
 													 response: OutgoingHTTPResponse): Promise<void> {
@@ -33,9 +35,20 @@ export async function defaultUnhandledRequestHandler(request: IncomingHTTPReques
 	
 }
 
+export async function defaultRequestFulfillmentChecker(request: IncomingHTTPRequest,
+													   response: OutgoingHTTPResponse): Promise<boolean> {
+	
+	return !request.getRoutingInfo().hasNextPathComponent();
+	
+}
+
 export class HTTPServer extends AbstractRouter {
 	
 	protected unhandledRequestHandler: MiddlewareFunction;
+	
+	protected requestFulfillmentChecker: MiddlewareFunction<boolean>;
+	
+	protected version: HTTPVersionObject;
 	
 	protected server: http.Server;
 	
@@ -44,7 +57,8 @@ export class HTTPServer extends AbstractRouter {
 		super();
 		
 		this.unhandledRequestHandler = defaultUnhandledRequestHandler;
-		
+		this.requestFulfillmentChecker = defaultRequestFulfillmentChecker;
+		this.version = { major: "1", minor: "1" };
 		this.server = http.createServer();
 		
 		this.server.on("request", (rawRequest: http.IncomingMessage, rawResponse: http.ServerResponse): void => {
@@ -84,6 +98,9 @@ export class HTTPServer extends AbstractRouter {
 	 * Normally, this consists of returning some form of an error to the user, but the actual functionality is
 	 * configurable by the end-user via {@link #setUnhandledRequestHandler}, so the actual functionality may vary.
 	 *
+	 * Typically, the functionality being provided by this function should match that which is being provided via the
+	 * {@link #checkRequestFulfillment} method.
+	 *
 	 * @param {IncomingHTTPRequest} request The request that has not been fulfilled.
 	 * @param {OutgoingHTTPResponse} response The response object matching to the associated request parameter.
 	 * @returns {Promise<void>} A Promise that resolves once the request has been dealt with.
@@ -96,15 +113,70 @@ export class HTTPServer extends AbstractRouter {
 	}
 	
 	/**
-	 * Sets the handler function to used to handle requests that have not been fulfilled as a part of the normal router
+	 * Sets the handler function to use to handle requests that have not been fulfilled as a part of the normal router
 	 * chain.
 	 *
-	 * @param {MiddlewareFunction} handler
+	 * Typically, the functionality being provided by the handler function should match that which is being provided
+	 * via the {@link #setRequestFulfillmentChecker} method.
+	 *
+	 * @param {MiddlewareFunction} handler The handler function to use to handle requests that have not been fulfilled
+	 * as a part of the normal router chain.
+	 * @see #handleUnhandledRequest
 	 */
 	public setUnhandledRequestHandler(handler: MiddlewareFunction): void {
 		
 		this.unhandledRequestHandler = handler;
 		
+	}
+	
+	/**
+	 * Returns a Promise that resolves to true if the provided request and response pair should be considered 'fully
+	 * routed', and should therefore be handled by the handler function associated with the last router to have received
+	 * the request.
+	 *
+	 * Typically, the functionality being provided by this function should match that which is being provided via the
+	 * {@link #handleUnhandledRequest} method.
+	 *
+	 * @param {IncomingHTTPRequest} request The request that is being checked as to whether or not it is fulfilled.
+	 * @param {OutgoingHTTPResponse} response The response object matching to the associated request parameter.
+	 * @returns {Promise<boolean>} A Promise that resolves to true if the provided request and response pair should be
+	 * considered 'fully routed'.
+	 * @see #setRequestFulfillmentChecker
+	 */
+	public async checkRequestFulfillment(request: IncomingHTTPRequest,
+										 response: OutgoingHTTPResponse): Promise<boolean> {
+		
+		return this.requestFulfillmentChecker(request, response);
+		
+	}
+	
+	/**
+	 * Sets the handler function to use to check whether or not a given request and response pair should be considered
+	 * 'fully routed'.
+	 *
+	 * Typically, the functionality being provided by the handler function should match that which is being provided
+	 * via the {@link #setUnhandledRequestHandler} method.
+	 *
+	 * @param {MiddlewareFunction<boolean>} handler The handler function to use to check whether or not a given request
+	 * and response pair should be considered 'fully routed'.
+	 * @see #checkRequestFulfillment
+	 */
+	public setRequestFulfillmentChecker(handler: MiddlewareFunction<boolean>): void {
+		
+		this.requestFulfillmentChecker = handler;
+		
+	}
+	
+	public async shouldRoute(request: IncomingHTTPRequest, response: OutgoingHTTPResponse): Promise<boolean> {
+		
+		return true;
+		
+	}
+	
+	public getHTTPVersion(): HTTPVersionObject {
+	
+		return this.version;
+	
 	}
 
 }
